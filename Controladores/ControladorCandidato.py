45fb2c79ae0aec48797b951c85d69245233776ec
yulibahamon <yulibahamon13@gmail.com>
2022-11-03 00:50:40 -0500

    modificacion link de db

--- Controladores/ControladorCandidato.py
+++ Controladores/ControladorCandidato.py
@@ -1,9 +1,11 @@
 from Modelos.Candidato import Candidato
 from Repositorios.RepositorioCandidato import RepositorioCandidato
-
+from Modelos.Partido import Partido
+from Repositorios.RepositorioPartido import RepositorioPartido
 class ControladorCandidato():
     def __init__(self):
         self.repositorioCandidato = RepositorioCandidato()
+        self.repositorioPartido = RepositorioPartido()
     def index(self):
         return self.repositorioCandidato.findAll()
     def create(self,infoCandidato):
@@ -22,3 +24,9 @@ class ControladorCandidato():
         return self.repositorioCandidato.save(CandidatoActual)
     def delete(self,id):
         return self.repositorioCandidato.delete(id)
+
+    def asignarPartido(self, id, id_partido):
+        candidatoActual = Candidato(self.repositorioCandidato.findById(id))
+        partidoActual = Partido(self.repositorioPartido.findById(id_partido))
+        candidatoActual.partido = partidoActual
+        return self.repositorioCandidato.save(candidatoActual)
